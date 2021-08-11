ef513fe4ea7344c84e6d0423dba7103f981be070
ishizukamaaya <ec2-user@ip-172-31-65-194.ec2.internal>
2021-08-11 01:06:01 +0000

    0811

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def index
+  end
+
+  private
+  def user_params
+    params.require(:user).premit(:image,:name,:introduction)
+  end
+end
