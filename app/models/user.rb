5433c7e8ef8e93f62794480aa9b713ead4a5a710
ishizukamaaya <maayajas@gmail.com>
2021-11-08 08:30:41 +0000

    follow-create

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,25 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   attachment :profile_image
+
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy #フォロー中を取得
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy #フォローを取得
+  has_many :following_user, through: :follower, source: :followed #フォロー中
+  has_many :follower_user, through: :followed, source: :follower #フォローワー
+
+  #フォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  #フォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  #フォローしているか確認
+  def following?(user)
+    following_user.incloude?(user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :books
-  resources :users, only:[:show,:edit,:index,:update]
+  resources :users, only:[:show,:edit,:index,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   root to:'homes#top'
   get 'home/about' => 'homes#about'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
 end
